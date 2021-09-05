22c36186ec83cedddf896d27fdf0ec0ae5a543ad
IFEOR <nazumy@bk.ru>
2021-09-05 12:14:10 +0600

    [~] Binding field (bind test)

--- app/src/main/java/com/ifeor/meme/MainActivity.kt
+++ app/src/main/java/com/ifeor/meme/MainActivity.kt
@@ -2,8 +2,12 @@ package com.ifeor.meme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ifeor.meme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
